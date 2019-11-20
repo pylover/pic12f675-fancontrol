73fa3ecdc4ed49a2d54eba13daca89cb05193146
pylover <vahid.mardani@gmail.com>
2019-11-21 02:41:08 +0330

    PWM, display and A2D is working

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 
 // BEGIN CONFIG
 #pragma config FOSC =  INTRCIO   // Oscillator Selection bits (RC oscillator: CLKOUT function on GP4/OSC2/CLKOUT pin, RC on GP5/OSC1/CLKIN)
-#pragma config WDTE =  ON        // Watchdog Timer Enable bit (WDT enabled)
+#pragma config WDTE =  OFF        // Watchdog Timer Enable bit (WDT enabled)
 #pragma config PWRTE = ON       // Power-Up Timer Enable bit (PWRT disabled)
 #pragma config MCLRE = OFF       // GP3/MCLR pin function select (GP3/MCLR pin function is MCLR)
 #pragma config BOREN = ON       // Brown-out Detect Enable bit (BOD enabled)
@@ -20,55 +20,51 @@
 #include "max7219.c"
 
 #define FAN GP5
-#define REF     5
-#define INTERVAL    10
 
-static unsigned short counter = 0;
-
-
-float adc() {
-    short adcvalue = (ADRESH << 8) + ADRESL;
-    float v = ((float)adcvalue) * REF;
-    return v / 1.024;
-}
+static int adcvalue = 0;
+static unsigned char duty = 0;
 
 
-void delayus(int us) {
-   while(us > 0) {
-       _delay(10);
-      us -= 10;
+void interrupt isr(void) {
+    if (ADIF) {
+        adcvalue = (ADRESH << 8) + ADRESL;
+        ADIF = 0;
+    } 
+    if (T0IF) {
+        if (FAN) {
+            TMR0 = duty;
+            FAN = 0;
+        }
+        else if (duty > 5) {
+            TMR0 = 0xff - duty;
+            FAN = 1;
+        }
+        T0IF = 0;
     }
 }
 
 
-
 int main() {
     TRISIO = 0b00000100;        // GP2: IN 
-    OPTION_REG = 0b11111111;
-    ANSEL = 0b01110100;         // OSC/16, GP2->AN2
+    OPTION_REG = 0b11010001;
+    ANSEL = 0b00010100;         // GP2->AN2
     ADCON0 = 0b10001001;        // ADON, AN2, VDD
     CMCON = 0b00000111;
-    //VRCON = 0b10101010;
-
-    float temp;
-    unsigned int duty;
+    GIE = 1;
+    ADIE = 1;
+    PEIE = 1;
+    ADIF = 0;
+    T0IE = 1;
+    TMR0 = 0;
+    T0IF = 0;
+    
     max7219_init();
+    GO_nDONE = 1;   // ADC enable
     while(1) {
-        if (counter % INTERVAL == 0) {
-            GO_nDONE = 1;   // ADC enable
-        }
-        if (!GO_nDONE) {
-            temp = adc();
-            duty = (unsigned int)(temp * 1000);
-            display((unsigned int)temp, 3);
-            //display(duty, 0);
-        }
-        counter++;
-        FAN = 0;
-        delayus((1000000 - duty));
-        FAN = 1;
-        delayus(duty);
-        CLRWDT();
+        duty = adcvalue / 4; 
+        display(duty, 3);
+        GO_nDONE = 1;   // ADC enable
+        _delay(1000000);
     }
     return 0;
 }
